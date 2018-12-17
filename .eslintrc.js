0a712074c75fc8cb0ed6b9f5402088c5fd12ece3
admin <admin@example.com>
2018-12-17 18:25:09 +0800

    add ajax

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
     "extends": "standard",
     "rules": {
         "comma-dangle": 0,
-        "no-console": 0
+        "no-console": 0,
     }
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 import '../sass/style.sass'
 import 'jquery'
+
+const api = 'https://jsonsweeteaste.herokuapp.com/menu'
+var menu = []
+
+$(document).ready(function () {
+	$.ajax({
+		type: 'GET',
+		url: api,
+		dataType: 'json',
+		success: function (res) {
+			menu.push(res)
+			console.log(menu)
+		}
+	})
+
+})
