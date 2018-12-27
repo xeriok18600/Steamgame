7b35fdf908481695004de7742345cf5798a8ab11
admin <admin@example.com>
2018-12-27 18:18:12 +0800

    Modify sliding display page

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import '../sass/style.sass'
 import 'jquery'
-
 import {
 	cardAdd,
 	cardSearch
@@ -12,40 +11,69 @@ import {
 } from './module/mouse'
 
 var imagesLoaded = require('imagesloaded')
+// var InfiniteScroll = require('infinite-scroll')
 var $ = require('jquery')
+
 imagesLoaded.makeJQueryPlugin($)
 
 const api = 'https://jsonsweeteaste.herokuapp.com/menu'
 var menu = []
+var page = 1
 // eslint-disable-next-line camelcase
 
-
-$(document).ready(function () {
-	$.ajax({
-		type: 'GET',
-		url: api,
-		dataType: 'json',
-		error: function () {
-			console.log('載入失敗')
-		},
-		success: function (res) {
-			menu = res
-			// console.log(menu)
-			$('#total__title').text(menu.length)
-			$('#card__group').imagesLoaded(function () {
-				cardAdd(menu)
-			})
-			cardSearch(menu)
-		}
-	})
+$.ajax({
+	type: 'GET',
+	url: api,
+	data: {
+		'page': page
+	},
+	dataType: 'json',
+	error: function () {
+		console.log('載入失敗')
+	},
+	success: function (res) {
+		page += 1
+		menu = res
+		console.log(menu)
+		$('#total__title').text(menu.length)
+		$('#card__group').imagesLoaded(function () {
+			cardAdd(menu)
+		})
+		cardSearch(menu)
+	}
 })
+
 mouseStatus()
 
-$(window).scroll(function () {
+$(window).data('ajaxready', true).scroll(function () {
 	// eslint-disable-next-line camelcase
 	var w_scrollTop = $(window).scrollTop()
 	scrollStatus(w_scrollTop)
 	console.log(w_scrollTop)
+
+	// var infScroll = new InfiniteScroll('#card__group', {
+	// 	path: testData
+	// })
+
+	// function testData() {
+	// 	$.ajax({
+	// 		type: 'GET',
+	// 		url: api,
+	// 		dataType: 'json',
+	// 		error: function () {
+	// 			console.log('載入失敗')
+	// 		},
+	// 		success: function (res) {
+	// 			menu = res
+	// 			console.log(menu)
+	// 			$('#total__title').text(menu.length)
+	// 			$('#card__group').imagesLoaded(function () {
+	// 				cardAdd(menu)
+	// 			})
+	// 			cardSearch(menu)
+	// 		}
+	// 	})
+	// }
 })
 
 clickTop()
--- src/js/module/card.js
+++ src/js/module/card.js
@@ -1,7 +1,7 @@
 export function cardAdd(menu) {
 	var displayInfo = $('#card__group')
 	for (var i = 0; i <= menu.length; i++) {
-		displayInfo.append('<div class="column is-3 "><div class="card"><div class="card-image"><figure class="image is-4by3"><img class="bg-p" src="' + menu[i].url + '"/></figure></div><div class="card-content"><div class="media"><div class="media-left"><figure class="image is-48x48"><img class="sm-p" src="' + menu[i].url + '"/></figure></div><div class="media-content"><p class="title is-4">' + menu[i].name + '</p><p class="subtitle is-6">$$$</p></div></div><div class="content">俗又大碗 </div><a href="#">#冒險</a><a href="#">#大型多人連線</a><br /><time datetime="">11:09 PM - 1 Jan 2016</time></div></div></div>')
+		displayInfo.append('<div class="column is-3 5566 "><div class="card"><div class="card-image"><figure class="image is-4by3"><img class="bg-p" src="' + menu[i].url + '"/></figure></div><div class="card-content"><div class="media"><div class="media-left"><figure class="image is-48x48"><img class="sm-p" src="' + menu[i].url + '"/></figure></div><div class="media-content"><p class="title is-4">' + menu[i].name + '</p><p class="subtitle is-6">$$$</p></div></div><div class="content">俗又大碗 </div><a href="#">#冒險</a><a href="#">#大型多人連線</a><br /><time datetime="">11:09 PM - 1 Jan 2016</time></div></div></div>')
 	}
 }
 // https://cdn.discordapp.com/attachments/161411050300899328/524866016552157215/LOAD.png
